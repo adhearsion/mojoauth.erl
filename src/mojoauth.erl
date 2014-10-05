9b9d5c079de7b5d06301ff6576ee16175bedc64b
Ben Langfeld <ben@langfeld.me>
2014-10-05 14:56:30 -0300

    Real implementation
    No testing of expiry yet

--- src/mojoauth.erl
+++ src/mojoauth.erl
@@ -4,17 +4,38 @@
 -export([
   create_secret/0,
   create_credentials/1,
-  test_credentials/1
+  create_credentials/2,
+  test_credentials/2
 ]).
 
 create_secret() ->
   base64:encode(crypto:strong_rand_bytes(64)).
 
-create_credentials({secret, Secret}) ->
+create_credentials({id, Id}, {secret, Secret}) ->
+  {Mega, Secs, _} = now(),
+  Timestamp = Mega*1000000 + Secs,
+  Username = string:join([integer_to_list(Timestamp), Id], ":"),
   [
-    {username, Secret},
-    {password, Secret}
+    {username, Username},
+    {password, sign(Username, Secret)}
   ].
 
-test_credentials([{username, Username}, {password, Password}]) ->
-  Username == Password.
+create_credentials({secret, Secret}) ->
+  create_credentials({id, ""}, {secret, Secret}).
+
+test_credentials([{username, Username}, {password, Password}], Secret) ->
+  case string:tokens(Username, ":") of
+    [_, Id] ->
+      test_signature(Username, Password, Secret, Id);
+    [_] ->
+      test_signature(Username, Password, Secret, true)
+  end.
+
+sign(Message, Secret) ->
+  base64:encode(crypto:hmac(sha, Secret, Message)).
+
+test_signature(Username, Password, Secret, RetVal) ->
+  case sign(Username, Secret) of
+    Password -> RetVal;
+    _ -> false
+  end.
--- test/mojoauth_tests.erl
+++ test/mojoauth_tests.erl
@@ -7,5 +7,36 @@ create_secret_test() ->
   88 = byte_size(Secret).
 
 created_credentials_test_true_test() ->
-  Credentials = mojoauth:create_credentials({secret, mojoauth:create_secret()}),
-  true = mojoauth:test_credentials(Credentials).
+  Secret = mojoauth:create_secret(),
+  Credentials = mojoauth:create_credentials({secret, Secret}),
+  true = mojoauth:test_credentials(Credentials, Secret).
+
+incorrect_password_tests_false_test() ->
+  Secret = mojoauth:create_secret(),
+  [{username, Username}, _] = mojoauth:create_credentials({secret, Secret}),
+  false = mojoauth:test_credentials([{username, Username}, {password, "foobar"}], Secret).
+
+different_secret_tests_false_test() ->
+  Secret1 = mojoauth:create_secret(),
+  Secret2 = mojoauth:create_secret(),
+  Credentials = mojoauth:create_credentials({secret, Secret1}),
+  false = mojoauth:test_credentials(Credentials, Secret2).
+
+asserted_id_created_credentials_return_id_test() ->
+  Id = "foobar",
+  Secret = mojoauth:create_secret(),
+  Credentials = mojoauth:create_credentials({id, Id}, {secret, Secret}),
+  Id = mojoauth:test_credentials(Credentials, Secret).
+
+asserted_id_incorrect_password_tests_false_test() ->
+  Id = "foobar",
+  Secret = mojoauth:create_secret(),
+  [{username, Username}, _] = mojoauth:create_credentials({id, Id}, {secret, Secret}),
+  false = mojoauth:test_credentials([{username, Username}, {password, "foobar"}], Secret).
+
+asserted_id_different_secret_tests_false_test() ->
+  Id = "foobar",
+  Secret1 = mojoauth:create_secret(),
+  Secret2 = mojoauth:create_secret(),
+  Credentials = mojoauth:create_credentials({id, Id}, {secret, Secret1}),
+  false = mojoauth:test_credentials(Credentials, Secret2).
