b89f8d0631c6d933ba697c59207bf2972a7a644a
Ben Langfeld <ben@langfeld.me>
2014-10-05 19:28:38 -0300

    API docs

--- src/mojoauth.erl
+++ src/mojoauth.erl
@@ -1,3 +1,15 @@
+%% @type id(Id) = {id, Id}
+%%      Id = string()
+%% @type ttl(Ttl) = {ttl, Ttl}
+%%      Ttl = integer()
+%% @type secret(Secret) = {secret, Secret}
+%%      Secret = string()
+%% @type username(Username) = {username, Username}
+%%      Username = string()
+%% @type password(Password) = {password, Password}
+%%      Password = string()
+%% @type credential(Username, Password) = list(username(), password())
+
 -module(mojoauth).
 -vsn("0.1.0").
 
@@ -9,9 +21,13 @@
   test_credentials/2
 ]).
 
+%% @spec create_secret() -> string()
+%% @doc Create a new random secret
 create_secret() ->
   base64:encode(crypto:strong_rand_bytes(64)).
 
+%% @spec create_credentials(Id::id(), Ttl::ttl(), Secret::secret()) -> credential()
+%% @doc Create a new set of credentials for an asserted ID, given a desired TTL and shared secret
 create_credentials({id, Id}, {ttl, Ttl}, {secret, Secret}) ->
   {Mega, Secs, _} = os:timestamp(),
   Timestamp = Mega*1000000 + Secs + Ttl,
@@ -21,12 +37,18 @@ create_credentials({id, Id}, {ttl, Ttl}, {secret, Secret}) ->
     {password, sign(Username, Secret)}
   ].
 
+%% @spec create_credentials(Id::id(), Secret::secret()) -> credential()
+%% @doc Create a new set of credentials for an asserted ID, given the default TTL of 1 day and shared secret
 create_credentials({id, Id}, {secret, Secret}) ->
   create_credentials({id, Id}, {ttl, 86400}, {secret, Secret}).
 
+%% @spec create_credentials(Secret::secret()) -> credential()
+%% @doc Create a new set of credentials for a shared secret without an asserted ID and the default TTL of 1 day
 create_credentials({secret, Secret}) ->
   create_credentials({id, ""}, {secret, Secret}).
 
+%% @spec test_credentials(Credential::credential(), secret()) -> any()
+%% @doc Test a set of credentials are valid for the given secret. When an identity is asserted, the identity is returned as a string, otherwise a boolean is returned.
 test_credentials([{username, Username}, {password, Password}], Secret) ->
   RetVal = case string:tokens(Username, ":") of
     [Timestamp, Id] ->
