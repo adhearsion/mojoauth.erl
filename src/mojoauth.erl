b0fe235e100e2999a32243a7c21dbc976a7995ff
Ben Langfeld <ben@langfeld.me>
2014-10-05 14:10:15 -0300

    Super simple framework for public API

--- src/mojoauth.erl
+++ src/mojoauth.erl
@@ -1,2 +1,20 @@
 -module(mojoauth).
 -vsn("0.1.0").
+
+-export([
+  create_secret/0,
+  create_credentials/1,
+  test_credentials/1
+]).
+
+create_secret() ->
+  base64:encode(crypto:strong_rand_bytes(64)).
+
+create_credentials({secret, Secret}) ->
+  [
+    {username, Secret},
+    {password, Secret}
+  ].
+
+test_credentials([{username, Username}, {password, Password}]) ->
+  Username == Password.
--- test/mojoauth_tests.erl
+++ test/mojoauth_tests.erl
@@ -1,1 +1,11 @@
 -module(mojoauth_tests).
+
+-compile(export_all).
+
+create_secret_test() ->
+  Secret = mojoauth:create_secret(),
+  88 = byte_size(Secret).
+
+created_credentials_test_true_test() ->
+  Credentials = mojoauth:create_credentials({secret, mojoauth:create_secret()}),
+  true = mojoauth:test_credentials(Credentials).
